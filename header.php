f42e74378d2498222ac37cf86fb8227e7f40c783
Mega Hwee <megahwee@gmail.com>
2022-03-02 22:22:08 +0800

    initial

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
     <title>Initio - Free, multipurpose html5 template by GetTemplate</title>
--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=0.1"> -->
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
     <title>Initio - Free, multipurpose html5 template by GetTemplate</title>
