9da75bdad5ba2593db783e35019e0ecab8296a54
Mega Hwee <megahwee@gmail.com>
2022-03-03 18:28:25 +0800

    initial

--- web/index.php
+++ web/index.php
@@ -1,25 +1,145 @@
-<?php
+<?php include 'header.php';?>
+<!DOCTYPE html>
+<html lang="en">
 
-require('../vendor/autoload.php');
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=1024">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="description" content="">
+    <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
+    <title>Initio - Free, multipurpose html5 template by GetTemplate</title>
+    <link rel="shortcut icon" href="assets/images/gt_favicon.png">
+    <!-- Bootstrap -->
+    <!-- <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet"> -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+    <!-- Icons -->
+    <!-- <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+</head>
 
-$app = new Silex\Application();
-$app['debug'] = true;
+<body class="home">
+    <main id="main">
+        <div class="container">
+            <div class="row section topspace">
+                <div class="col-md-12">
+                    <p class="lead text-center text-muted">Let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. you <a href="about.php">measure</a> yourself by the people who measure themselves by you. it only took me <a href="sidebar-right.php">six days</a>. </p>
+                </div>
+            </div> <!-- / section -->
+            <div class="row section recentworks topspace">
+                <h2 class="section-title"><span>Portfolio</span></h2>
+                <div class="thumbnails recentworks row">
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Sample title - big data solutions</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Wordpress</a> | <a href="">Logotype</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Pure ipsum - development services for people</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Wordpress</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Lorem studios - interior and patio design</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Logotype</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Pure ipsum - development services for people</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Wordpress</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Lorem studios - interior and patio design</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Logotype</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <a class="thumbnail" href="sidebar-right.php">
+                            <span class="img">
+                                <img src="assets/images/s1.jpg" alt="">
+                                <span class="cover"><span class="more">See details &rarr;</span></span>
+                            </span>
+                            <span class="title">Lorem studios - interior and patio design</span>
+                        </a>
+                        <span class="details"><a href="">Web design</a> | <a href="">Logotype</a></span>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                </div>
+            </div> <!-- /section -->
+            <div class="row section featured topspace">
+                <h2 class="section-title"><span>Services</span></h2>
+                <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                        <h3 class="text-center">Custom website design</h3>
+                        <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
+                        <p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <h3 class="text-center">Wordpress integration</h3>
+                        <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
+                        <p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <h3 class="text-center">Application development</h3>
+                        <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
+                        <p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                        <h3 class="text-center">SEO &amp; SEM services</h3>
+                        <p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
+                        <p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
+                    </div>
+                </div>
+            </div> <!-- / section -->
+        </div> <!-- /container -->
+    </main>
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="assets/js/template.js"></script>
+</body>
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+</html>
+<?php include 'footer.php';?>
