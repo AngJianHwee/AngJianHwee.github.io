5e26bdf04f801bb2c057a24691748560888e262e
Mega Hwee <megahwee@gmail.com>
2022-03-05 19:10:40 +0800

    initial

--- certifications.php
+++ certifications.php
@@ -1,141 +1,137 @@
 <?php include 'header.php';?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
-    <title>Certifications</title>
-    <link rel="shortcut icon" href="assets/images/gt_favicon.png">
-    <!-- Custom -->
-    <!-- Bootstrap -->
-    <!-- <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet"> -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
-    <!-- Icons -->
-    <!-- <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,600">
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
-</head>
-
-<body class="home">
-    <main id="main">
-        <div class="container">
-                        <h2 class="section-title"><span>Certification</span></h2>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
+      <title>Certifications</title>
+      <link rel="shortcut icon" href="assets/images/gt_favicon.png">
+      <!-- Custom -->
+      <!-- Bootstrap -->
+      <!-- <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet"> -->
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+      <!-- Icons -->
+      <!-- <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> -->
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+      <!-- Fonts -->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,600">
+      <!-- Custom styles -->
+      <link rel="stylesheet" href="assets/css/styles.css">
+      <!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+   </head>
+   <body class="home">
+      <main id="main">
+         <div class="container">
+         <h2 class="section-title"><span>Certification</span></h2>
+         <div class="table-responsive">
+            <table class="table custom-table">
+               <thead>
+                  <tr>
+                     <th width = 100 scope="col">Order</th>
+                     <th width = 600 scope="col">Certificate</th>
+                     <th scope="col">Provider</th>
+                     <!-- <th scope="col">Education</th> -->
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr scope="row">
+                     <td>01</td>
+                     <td><a href="certificate/Oracle Certified Foundations Associate.pdf">Oracle Certified Foundations Associate</a></td>
+                     <td>Oracle</td>
+                  </tr>
+               </tbody>
+            </table>
+            <br>
+            <br>
+            <h2 class="section-title"><span>Guided Project</span></h2>
             <div class="table-responsive">
-                <table class="table custom-table">
-    <thead>
-        <tr>
-            <th width = 100 scope="col">Order</th>
-            <th width = 600 scope="col">Certificate</th>
-            <th scope="col">Provider</th>
-            <!-- <th scope="col">Education</th> -->
-        </tr>
-    </thead>
-    <tbody>
-        <tr scope="row">
-            <td>01</td>
-            <td><a href="certificate/Oracle Certified Foundations Associate.pdf">Oracle Certified Foundations Associate</a>
-            </td>
-            <td>Oracle</td>
-        </tr>
-    </tbody>
-</table>
-<br>
-<br>
-<h2 class="section-title"><span>MOOC</span></h2>
-<div class="table-responsive">
-    <table class="table custom-table">
-        <thead>
-            <tr>
-                <th width = 100 scope="col">Order</th>
-                <th width = 600 scope="col">Course</th>
-                <th scope="col">Provider</th>
-                <!-- <th scope="col">Education</th> -->
-            </tr>
-        </thead>
-        <tbody>
-            <tr scope="row">
-                <td>01</td>
-                <td><a href="https://coursera.org/verify/CX5YPY6LPPQ7">Convolutional Neural Networks in TensorFlowTensorFlow</a>
-                </td>
-                <td>DeepLearning.AI, Coursera</td>
-            </tr>
-            <tr scope="row">
-                <td>02</td>
-                <td><a href="https://coursera.org/verify/LYQ7WYA2ZHNB">Natural Language Processing in TensorFlow</a>
-                </td>
-                <td>DeepLearning.AI, Coursera</td>
-                <td>
-            </tr>
-            <tr scope="row">
-                <td>03</td>
-                <td> <a href="https://coursera.org/verify/WL3N772CKBJP">Foundations of Project Management</a>
-                </td>
-                <td>Google, Coursera</td>
-                <td>
-            </tr>
-            <tr scope="row">
-                <td>04</td>
-                <td><a href="https://coursera.org/verify/FCLKM35T5ERF">Using Python to Access Web Data</a>
-                </td>
-                <td>University of Michigan, Coursera</td>
-            </tr>
-            <tr scope="row">
-                <td>05</td>
-                <td><a href="https://coursera.org/verify/2ZJEPUFDDYLR">Coursera-Intro to Analytic Thinking, Data Science and Data Mining</a>
-                </td>
-                <td>University of California, Irvine, Coursera</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-<br>
-<br>
-<h2 class="section-title"><span>Guided Project</span></h2>
-<div class="table-responsive">
-    <table class="table custom-table">
-        <thead>
-            <tr>
-                <th width = 100 scope="col">Order</th>
-                <th width = 600 scope="col">Project</th>
-                <th scope="col">Provider</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr scope="row">
-                <td>01</td>
-                <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Fine Tune BERT for Text Classification with TensorFlow</a>
-                </td>
-                <td>Coursera Project Network</td>
-            </tr>
-            <tr scope="row">
-                <td>02</td>
-                <td><a href="https://coursera.org/verify/96PJXB843KQ5">Tweet Emotion Recognition with TensorFlow</a>
-                </td>
-                <td>Coursera Project Network</td>
-            </tr>
-            <tr scope="row">
-                <td>03</td>
-                <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Visualizing Filters of a CNN using TensorFlow</a>
-                </td>
-                <td>Coursera Project Network</td>
-            </tr>
-        </tbody>
-    </table>
+               <table class="table custom-table">
+                  <thead>
+                     <tr>
+                        <th width = 100 scope="col">Order</th>
+                        <th width = 600 scope="col">Project</th>
+                        <th scope="col">Provider</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr scope="row">
+                        <td>01</td>
+                        <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Fine Tune BERT for Text Classification with TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                     </tr>
+                     <tr scope="row">
+                        <td>02</td>
+                        <td><a href="https://coursera.org/verify/96PJXB843KQ5">Tweet Emotion Recognition with TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                     </tr>
+                     <tr scope="row">
+                        <td>03</td>
+                        <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Visualizing Filters of a CNN using TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                     </tr>
+                  </tbody>
+               </table>
+               <br>
+               <br>
+               <h2 class="section-title"><span>MOOC</span></h2>
+               <div class="table-responsive">
+                  <table class="table custom-table">
+                     <thead>
+                        <tr>
+                           <th width = 100 scope="col">Order</th>
+                           <th width = 600 scope="col">Course</th>
+                           <th scope="col">Provider</th>
+                           <!-- <th scope="col">Education</th> -->
+                        </tr>
+                     </thead>
+                     <tbody>
+                        <tr scope="row">
+                           <td>01</td>
+                           <td><a href="https://coursera.org/verify/CX5YPY6LPPQ7">Convolutional Neural Networks in TensorFlowTensorFlow</a>
+                           </td>
+                           <td>DeepLearning.AI, Coursera</td>
+                        </tr>
+                        <tr scope="row">
+                           <td>02</td>
+                           <td><a href="https://coursera.org/verify/LYQ7WYA2ZHNB">Natural Language Processing in TensorFlow</a>
+                           </td>
+                           <td>DeepLearning.AI, Coursera</td>
+                           <td>
+                        </tr>
+                        <tr scope="row">
+                           <td>03</td>
+                           <td> <a href="https://coursera.org/verify/WL3N772CKBJP">Foundations of Project Management</a>
+                           </td>
+                           <td>Google, Coursera</td>
+                           <td>
+                        </tr>
+                        <tr scope="row">
+                           <td>04</td>
+                           <td><a href="https://coursera.org/verify/FCLKM35T5ERF">Using Python to Access Web Data</a>
+                           </td>
+                           <td>University of Michigan, Coursera</td>
+                        </tr>
+                        <tr scope="row">
+                           <td>05</td>
+                           <td><a href="https://coursera.org/verify/2ZJEPUFDDYLR">Coursera-Intro to Analytic Thinking, Data Science and Data Mining</a>
+                           </td>
+                           <td>University of California, Irvine, Coursera</td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>
             </div>
-        </div>
-    </main>
-    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="assets/js/template.js"></script>
-</body>
-
+         </div>
+      </main>
+      <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+      <script src="assets/js/template.js"></script>
+   </body>
 </html>
 <?php include 'footer.php';?>
