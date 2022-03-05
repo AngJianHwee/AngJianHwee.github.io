fb7685a86694cf0b37e043fe5abefa1e08be4d52
Mega Hwee <megahwee@gmail.com>
2022-03-06 01:34:29 +0800

    initial

--- certifications.php
+++ certifications.php
@@ -161,9 +161,6 @@
                     </tr>
                 </tbody>
             </table>
-            <br>
-            <br>
-            <br>
     </main>
     <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
