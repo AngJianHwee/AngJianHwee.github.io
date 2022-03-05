134619772111d20224712b528d2969cae7ee0327
Mega Hwee <megahwee@gmail.com>
2022-03-06 00:47:15 +0800

    initial

--- certifications.php
+++ certifications.php
@@ -28,122 +28,116 @@
         <br>
         <div class="container">
             <h2 class="section-title"><span>Certification</span></h2>
-            <div class="table-responsive">
-                <table class="table custom-table">
-                    <thead>
-                        <tr>
-                            <th width=100 scope="col">Order</th>
-                            <th width=600 scope="col">Certificate</th>
-                            <th scope="col">Provider</th>
-                            <!-- <th scope="col">Education</th> -->
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr scope="row">
-                            <td>01</td>
-                            <td><a href="certificate/Oracle Certified Foundations Associate.pdf">Oracle Certified Foundations Associate</a></td>
-                            <td>Oracle</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <br>
-                <br>
-                <h2 class="section-title"><span>Guided Project</span></h2>
-                <div class="table-responsive">
-                    <table class="table custom-table">
-                        <thead>
-                            <tr>
-                                <th width=100 scope="col">Order</th>
-                                <th width=600 scope="col">Project</th>
-                                <th scope="col">Provider</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr scope="row">
-                                <td>01</td>
-                                <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Fine Tune BERT for Text Classification with TensorFlow</a>
-                                </td>
-                                <td>Coursera Project Network</td>
-                            </tr>
-                            <tr scope="row">
-                                <td>02</td>
-                                <td><a href="https://coursera.org/verify/96PJXB843KQ5">Tweet Emotion Recognition with TensorFlow</a>
-                                </td>
-                                <td>Coursera Project Network</td>
-                            </tr>
-                            <tr scope="row">
-                                <td>03</td>
-                                <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Visualizing Filters of a CNN using TensorFlow</a>
-                                </td>
-                                <td>Coursera Project Network</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <br>
-                    <br>
-                    <h2 class="section-title"><span>MOOC</span></h2>
-                    <div class="table-responsive">
-                        <table class="table custom-table">
-                            <thead>
-                                <tr>
-                                    <th width=100 scope="col">Order</th>
-                                    <th width=600 scope="col">Course</th>
-                                    <th scope="col">Provider</th>
-                                    <!-- <th scope="col">Education</th> -->
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr scope="row">
-                                    <td>01</td>
-                                    <td><a href="https://coursera.org/verify/CX5YPY6LPPQ7">Convolutional Neural Networks in TensorFlowTensorFlow</a>
-                                    </td>
-                                    <td>DeepLearning.AI on Coursera</td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>02</td>
-                                    <td><a href="https://coursera.org/verify/LYQ7WYA2ZHNB">Natural Language Processing in TensorFlow</a>
-                                    </td>
-                                    <td>DeepLearning.AI on Coursera</td>
-                                    <td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>03</td>
-                                    <td> <a href="https://coursera.org/verify/WL3N772CKBJP">Foundations of Project Management</a>
-                                    </td>
-                                    <td>Google on Coursera</td>
-                                    <td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>04</td>
-                                    <td><a href="https://coursera.org/verify/FCLKM35T5ERF">Using Python to Access Web Data</a>
-                                    </td>
-                                    <td>University of Michigan on Coursera</td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>05</td>
-                                    <td><a href="https://coursera.org/verify/2ZJEPUFDDYLR">Coursera-Intro to Analytic Thinking, Data Science and Data Mining</a>
-                                    </td>
-                                    <td>University of California, Irvine on Coursera</td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>06</td>
-                                    <td><a href="https://coursera.org/verify/AKRD46K8XM4H">Cloud Data Engineering
-                                        </a>
-                                    </td>
-                                    <td>Duke University on Coursera</td>
-                                </tr>
-                                <tr scope="row">
-                                    <td>07</td>
-                                    <td><a href="https://coursera.org/verify/SY4NEGQXXBA9">Introduction to HTML5
-                                        </a>
-                                    </td>
-                                    <td>University of Michigan on Coursera</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+            <table class="table custom-table">
+                <thead>
+                    <tr>
+                        <th width=100 scope="col">Order</th>
+                        <th width=600 scope="col">Certificate</th>
+                        <th scope="col">Provider</th>
+                        <!-- <th scope="col">Education</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr scope="row">
+                        <td>01</td>
+                        <td><a href="certificate/Oracle Certified Foundations Associate.pdf">Oracle Certified Foundations Associate</a></td>
+                        <td>Oracle</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <br>
+            <h2 class="section-title"><span>Guided Project</span></h2>
+            <table class="table custom-table">
+                <thead>
+                    <tr>
+                        <th width=100 scope="col">Order</th>
+                        <th width=600 scope="col">Project</th>
+                        <th scope="col">Provider</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr scope="row">
+                        <td>01</td>
+                        <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Fine Tune BERT for Text Classification with TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>02</td>
+                        <td><a href="https://coursera.org/verify/96PJXB843KQ5">Tweet Emotion Recognition with TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>03</td>
+                        <td><a href="https://coursera.org/verify/9JQB7FMTRXN6">Visualizing Filters of a CNN using TensorFlow</a>
+                        </td>
+                        <td>Coursera Project Network</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <br>
+            <h2 class="section-title"><span>MOOC</span></h2>
+            <table class="table custom-table">
+                <thead>
+                    <tr>
+                        <th width=100 scope="col">Order</th>
+                        <th width=600 scope="col">Course</th>
+                        <th scope="col">Provider</th>
+                        <!-- <th scope="col">Education</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr scope="row">
+                        <td>01</td>
+                        <td><a href="https://coursera.org/verify/CX5YPY6LPPQ7">Convolutional Neural Networks in TensorFlowTensorFlow</a>
+                        </td>
+                        <td>DeepLearning.AI on Coursera</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>02</td>
+                        <td><a href="https://coursera.org/verify/LYQ7WYA2ZHNB">Natural Language Processing in TensorFlow</a>
+                        </td>
+                        <td>DeepLearning.AI on Coursera</td>
+                        <td>
+                    </tr>
+                    <tr scope="row">
+                        <td>03</td>
+                        <td> <a href="https://coursera.org/verify/WL3N772CKBJP">Foundations of Project Management</a>
+                        </td>
+                        <td>Google on Coursera</td>
+                        <td>
+                    </tr>
+                    <tr scope="row">
+                        <td>04</td>
+                        <td><a href="https://coursera.org/verify/FCLKM35T5ERF">Using Python to Access Web Data</a>
+                        </td>
+                        <td>University of Michigan on Coursera</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>05</td>
+                        <td><a href="https://coursera.org/verify/2ZJEPUFDDYLR">Coursera-Intro to Analytic Thinking, Data Science and Data Mining</a>
+                        </td>
+                        <td>University of California, Irvine on Coursera</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>06</td>
+                        <td><a href="https://coursera.org/verify/AKRD46K8XM4H">Cloud Data Engineering
+                            </a>
+                        </td>
+                        <td>Duke University on Coursera</td>
+                    </tr>
+                    <tr scope="row">
+                        <td>07</td>
+                        <td><a href="https://coursera.org/verify/SY4NEGQXXBA9">Introduction to HTML5
+                            </a>
+                        </td>
+                        <td>University of Michigan on Coursera</td>
+                    </tr>
+                </tbody>
+            </table>
     </main>
     <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
